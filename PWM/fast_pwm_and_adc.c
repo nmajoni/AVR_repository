6003d2919b85da7bad6086609d5dba4fb850c659
Nyasha Majoni <ip5jones@gmail.com>
2017-02-26 23:58:32 +0100

    controlling the speed of a motor using a potty

---
+++ PWM/fast_pwm_and_adc.c
@@ -0,0 +1,57 @@
+/*
+ * Controlling the speed of a motor using a potentiometer.
+ * Microcontroller used: ATMEGA 2560
+ * Created: 26-Feb-17 12:18:00
+ * Author : nyasha majoni
+ */ 
+
+#include <avr/io.h>
+#include "avr/interrupt.h"
+#include "util/delay.h"
+
+void fast_pwm_init()
+{
+	TCCR0A |= (1u << COM0A1)|(1u << WGM01)|(1u << WGM00); //Non-inveting mode and fast PWM selected
+	TCCR0B |= (1u << CS00); //no prescaller
+}
+
+void adc_init()
+{
+	ADMUX   |= (1u << REFS0); //AVCC with external capacitor at AREF pin. I am going to use ADC0
+	ADCSRA  |= (1u << ADEN)|(1u << ADPS0)|(1u << ADPS1)|(1u << ADPS2); //Enable ADC and set prescale of 128 so 125000Hz
+}
+
+uint16_t adc_val()
+{
+	ADCSRA |= (1u << ADSC);       //Start conversion
+	while(ADCSRA & (1u << ADSC)); //Wait for conversion to complete. Avoid this for production grade code. Interrupts are better
+	return (ADC);
+}
+
+//main function
+int main(void)
+{
+    DDRB |= (1u << PINB7); //set OC0A pin as output 
+	fast_pwm_init();       
+	adc_init();
+	
+    while (1) 
+    {
+
+		OCR0A = adc_val()/4;  //maping (0 - 255) to (0 - 1023) 
+		/*
+		//Increasing and decreasing LED brightness
+		for (brightness = 0; brightness < 255; brightness++)
+		{
+			OCR0A = brightness;
+			_delay_ms(5);
+		}
+		for (brightness = 255; brightness > 0; brightness--)
+		{
+			OCR0A = brightness;
+			_delay_ms(5);
+		}
+		*/
+    }
+}
+
