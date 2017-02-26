f8838a1d68ba1c7b572410a415867774c402edb1
Nyasha Majoni <ip5jones@gmail.com>
2017-02-27 00:04:14 +0100

    init

---
+++ ADC/ADC.c
@@ -0,0 +1,33 @@
+/*
+ * ADC.c
+ * ATMEGA 2560
+ * Created: 25-Feb-17 16:39:22
+ * Author : nmajo
+ */ 
+
+#include <avr/io.h>
+
+void adc_init()
+{
+	ADMUX   |= (1u << REFS0); //AVCC with external capacitor at AREF pin. I am going to use ADC0 
+	ADCSRA  |= (1u << ADEN)|(1u << ADPS0)|(1u << ADPS1)|(1u << ADPS2); //Enable ADC and set prescale of 128 so 125000Hz
+}
+
+uint16_t adc_val()
+{
+	ADCSRA |= (1u << ADSC);       //Start conversion
+	while(ADCSRA & (1u << ADSC)); //Wait for conversion to complete. Avoid this for production grade code. Interrupts are better
+	return (ADC);	
+}
+
+int main(void)
+{
+	volatile uint16_t adc_result;
+	adc_init();
+	
+    while (1) 
+    {
+		adc_result = adc_val();  
+    }
+}
+
